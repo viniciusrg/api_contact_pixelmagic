4a8136d489edb142a9feffd5c8d68a5352f2025b
Vinicius Goulart <viniciusrg10@msn.com>
2023-10-07 18:57:18 -0300

    remoção da rota get

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('api/')->group((function () {
 
     Route::post('/setContact', [ContactController::class, 'store']);
-    Route::get('/getContact', [ContactController::class, 'index']);
+    // Route::get('/getContact', [ContactController::class, 'index']);
 
 }));
