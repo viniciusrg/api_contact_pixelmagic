78a870dc126dbd3f38af0b5288cea06a690cda1e
Vinicius Goulart <viniciusrg10@msn.com>
2023-10-07 18:00:20 -0300

    Remoção de rotas inutilizadas

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('api/')->group((function () {
 
-    Route::get('/', function (){
-        echo '2ojkfodj';
-    });
-
     Route::post('/setContact', [ContactController::class, 'store']);
     Route::get('/getContact', [ContactController::class, 'index']);
 
