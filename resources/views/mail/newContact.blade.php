1f364eda9772355ba41b5461fb6310f67de1862f
Vinícius <viniciusrg10@msn.com>
2023-11-20 20:43:54 -0300

    Apenas um teste de commit

--- resources/views/mail/newContact.blade.php
+++ resources/views/mail/newContact.blade.php
@@ -7,6 +7,7 @@
             font-family: 'Arial', sans-serif;
             background-color: #f4f4f4;
             /* color: #333; */
+            /* teste */
             margin: 20px;
         }
 
